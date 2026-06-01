7f28aee5b29d249d285e61ff4d481bb774a0da72
Akshit Agrawal <akshitisagrawal@gmail.com>
2026-06-01 09:05:14 +0530

    setup

--- lexer.py
+++ lexer.py
@@ -1,5 +1,15 @@
-tokens = []
-code = 'if x > 5 print lol'
-for word in code.split():
-    tokens.append(word)
-print(tokens)
+KEYWORDS = ['if', 'print', 'while', 'else']
+OPERATORS = ['>', '<', '=', '+', '-', '*', '/']
+
+def tokenize(code):
+    tokens = []
+    for word in code.split():
+        if word.isdigit():
+            tokens.append(('digit',word))
+        if word in KEYWORDS:
+            tokens.append(('keyword',word))
+        if word in OPERATORS:
+            tokens.append(('operator',word))
+        else:
+            tokens.append(('identifier',word))
+    return tokens
